f09856de18b3ada79b25d2fc46bc9f71883e0343
fansurirazak <45352643+fansurirazak@users.noreply.github.com>
2022-06-16 13:39:59 +0800

    Add project files.

---
+++ IdentityTesting/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using IdentityTesting.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityTesting.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course>? Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ IdentityTesting/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityTesting.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        public string FirstName { get; set; } = string.Empty;
+        [PersonalData]
+        public string LastName { get; set; } = string.Empty;
+        [PersonalData]
+        public string IC { get; set; } = string.Empty;
+        public int UsernameChangeLimit { get; set; } = 10;
+        public byte[]? ProfilePicture { get; set; }
+    }
+}
